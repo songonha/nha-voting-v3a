22c12648ba92fe57f08821e7e21929ed7755aca8
Songonha <105191398+songonha@users.noreply.github.com>
2023-07-30 23:52:50 +0700

    Update service.js

--- service/service.js
+++ service/service.js
@@ -4,7 +4,7 @@ import VotingContract from '../abi/VotingContract.json'
 export const tokenContractInstance = (web3) => {
     return new web3.eth.Contract(
         TokenContract.abi, // abi of SC voting token
-        "0xa9A459eB931ABADC5ba38DbcDA9c6DFf41F7BA37" // address of Voting token
+        "0x246A40dba4633c668A7cd995c460829505c05BCD" // address of Voting token
     )
 }
 
@@ -12,6 +12,6 @@ export const votingContractInstance = web3 => {
     return new web3.eth.Contract(
         VotingContract.abi, // abi of SC governance contract
     //    "0x3c66F21096E319e57380A41A64D974cCA565387a"  // always false;
-        "0xe2b8A086e606bd2D41192C53eE9b69423fD88803" //;
+        "0x8CD769e46cc3782FDD1d545980be9be02eac4d4c" //;
     )
 }
